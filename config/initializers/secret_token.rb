cab4555f1de91ad02af2e59b5405a7b0229c3a40
julien3437 <julien_3437@hotmail.com>
2013-11-21 11:45:26 +0100

    PREMIER COMMIT NEW SERVEUR

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NewWebServeur::Application.config.secret_key_base = 'bd8cf26f2f9134ea076010a8e626b1f73c445b1bee6b0a1a2e38d860be3d5312fd0e57d6d7bb1bb36664e5af325dc30fa21978ed88404f015703ed0e7362fbca'
